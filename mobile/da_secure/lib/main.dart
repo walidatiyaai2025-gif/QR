7b62a60788d3c640f59dd61a30096a2f3942b8e6
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-26 00:13:29 +0300

    fix(mobile): use fail-closed Firebase initialization

--- mobile/da_secure/lib/main.dart
+++ mobile/da_secure/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await FirebaseBootstrap.initializeClientIfConfigured();
+  await FirebaseBootstrap.initializeClient();
   runApp(const DaSecureApp());
 }
 
